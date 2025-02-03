24f22d0346dc0c882c4261c3f196ad680c70f7ea
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-03 21:14:35 +0900

    q_11718

---
+++ src/class5/q_11718.java
@@ -0,0 +1,15 @@
+package class5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q_11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while((s = br.readLine())!=null){
+            System.out.println(s);
+        }
+    }
+}
