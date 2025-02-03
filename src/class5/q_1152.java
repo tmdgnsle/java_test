aa82706fa75af2426ce08ef7ca6d31f694bc4a3d
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-03 20:52:40 +0900

    q_1152

---
+++ src/class5/q_1152.java
@@ -0,0 +1,15 @@
+package class5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q_1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        System.out.println(input.length);
+    }
+}
