5f70c4e6245d59df9a2adf60b19e87a461d0a961
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-03 20:33:24 +0900

    q_9086

---
+++ src/class5/q_9086.java
@@ -0,0 +1,19 @@
+package class5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q_9086 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            String s = br.readLine();
+
+            System.out.println(""+s.charAt(0) + s.charAt(s.length()-1));
+        }
+    }
+}
