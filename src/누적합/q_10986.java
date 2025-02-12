9feb3a28cb261fcdbb39de068862d2dd35a21947
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-12 11:28:02 +0900

    q_10986

--- src/누적합/q_10986.java
+++ src/누적합/q_10986.java
@@ -8,30 +8,31 @@ public class q_10986 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
-		
+
 		long[] S = new long[N];
-		long[] C = new long[N];
-		
+		long[] C = new long[M];
+
 		long answer = 0;
-		
+
 		S[0] = sc.nextLong();
 		for (int i = 1; i < N; i++) {
-			S[i] = S[i-1] + sc.nextLong();
+			S[i] = S[i - 1] + sc.nextLong();
 		}
-		
-		for(int i=0; i<N; i++) {
-			int remainder = (int) (S[i]%M);
-			if(remainder == 0) answer++;
-			
+
+		for (int i = 0; i < N; i++) {
+			int remainder = (int) (S[i] % M);
+			if (remainder == 0)
+				answer++;
+
 			C[remainder]++;
 		}
-		
-		for(int i=0; i<N; i++) {
-			if(C[i] > 1) {
-				answer += (C[i] * (C[i]-1)) /2;
+
+		for (int i = 0; i < M; i++) {
+			if (C[i] > 1) {
+				answer += (C[i] * (C[i] - 1)) / 2;
 			}
 		}
-		
+
 		System.out.println(answer);
 
 	}
