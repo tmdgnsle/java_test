cac0b5be5d68118a81fb835d94306ced8c1fe439
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-12 09:02:19 +0900

    q_10986

---
+++ src/누적합/q_10986.java
@@ -0,0 +1,39 @@
+package 누적합;
+
+import java.util.Scanner;
+
+public class q_10986 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		long[] S = new long[N];
+		long[] C = new long[N];
+		
+		long answer = 0;
+		
+		S[0] = sc.nextLong();
+		for (int i = 1; i < N; i++) {
+			S[i] = S[i-1] + sc.nextLong();
+		}
+		
+		for(int i=0; i<N; i++) {
+			int remainder = (int) (S[i]%M);
+			if(remainder == 0) answer++;
+			
+			C[remainder]++;
+		}
+		
+		for(int i=0; i<N; i++) {
+			if(C[i] > 1) {
+				answer += (C[i] * (C[i]-1)) /2;
+			}
+		}
+		
+		System.out.println(answer);
+
+	}
+
+}
