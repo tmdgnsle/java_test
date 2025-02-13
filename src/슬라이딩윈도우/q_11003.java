1611ea9937a935b1ad912f9752a15c00e423ee9a
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-13 17:35:04 +0900

    q_11003

--- src/슬라이딩윈도우/q_11003.java
+++ src/슬라이딩윈도우/q_11003.java
@@ -1,5 +1,53 @@
 package 슬라이딩윈도우;
 
-public class q_11003 {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
+import org.w3c.dom.Node;
+
+public class q_11003 {
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
+		Deque<Node> mydeque = new LinkedList<>();
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i< N; i++) {
+			int now = Integer.parseInt(st.nextToken());
+			
+			while(!mydeque.isEmpty() && mydeque.getLast().value > now) {
+				mydeque.removeLast();
+			}
+			mydeque.addLast(new Node(now, i));
+			
+			if(mydeque.getFirst().index <= i - L) {
+				mydeque.removeFirst();
+			}
+			bw.write(mydeque.getFirst().value + " ");
+		}
+		bw.flush();
+		bw.close();
+		
+	}
+	
+	static class Node {
+		public int value;
+		public int index;
+		
+		Node(int value, int index){
+			this.value = value;
+			this.index = index;
+		}
+	}
 }
