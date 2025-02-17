155f3f439c478eda582655700d2eb2bb1d172302
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-17 17:09:56 +0900

    q_2023

---
+++ src/dfs/q_2023.java
@@ -0,0 +1,44 @@
+package dfs;
+
+import java.util.*;
+import java.io.*;
+
+
+public class q_2023 {
+	static int N;
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		DFS(2, 1);
+		DFS(3, 1);
+		DFS(5, 1);
+		DFS(7, 1);
+	}
+	
+	static void DFS(int number, int jarisu) {
+		if(jarisu == N) {
+			if(isPrime(number)) {
+				System.out.println(number);
+			}
+			return;
+		}
+		for(int i = 1; i< 10; i++) {
+			if (i%2 == 0) {
+				continue;
+			}
+			if(isPrime(number * 10 + i)) {
+				DFS(number * 10 + i, jarisu+1);
+			}
+		}
+	}
+	
+	static boolean isPrime(int num) {
+		for(int i = 2; i<=num/2; i++) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+}
