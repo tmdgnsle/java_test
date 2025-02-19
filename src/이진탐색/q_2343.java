51bc38b7688eebdc99b03c34d4d466b30fd1a54a
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-19 16:40:03 +0900

    q_2343

--- src/이진탐색/q_2343.java
+++ src/이진탐색/q_2343.java
@@ -1,5 +1,40 @@
 package 이진탐색;
 
-public class q_2343 {
+import java.util.*;
+import java.io.*;
 
+public class q_2343 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] A = new int[N];
+		int start = 0;
+		int end = 0;
+		for (int i = 0; i < N; i++) {
+			A[i] = sc.nextInt();
+			if (start < A[i])
+				start = A[i];
+			end += A[i];
+		}
+		while (start <= end) {
+			int middle = (start + end) / 2;
+			int sum = 0;
+			int count = 0;
+			for (int i = 0; i < N; i++) {
+				if (sum + A[i] > middle) {
+					count++;
+					sum = 0;
+				}
+				sum = sum + A[i];
+			}
+			if (sum != 0)
+				count++;
+			if (count > M)
+				start = middle + 1;
+			else
+				end = middle - 1;
+		}
+		System.out.println(start);
+	}
 }
