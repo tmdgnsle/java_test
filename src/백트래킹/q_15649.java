32fc2259dd51e1f3ba640568e9d2fc975ccc1d13
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-20 13:04:15 +0900

    q_15649

---
+++ src/백트래킹/q_15649.java
@@ -0,0 +1,43 @@
+package 백트래킹;
+
+import java.util.*;
+import java.io.*;
+
+public class q_15649 {
+	static int N;
+	static int M;
+	static ArrayList<Integer> result;
+	static boolean[] v;
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		v = new boolean[N + 1];
+		result = new ArrayList<>();
+		bt();
+	}
+
+	static void bt() {
+		if (result.size() == M) {
+			// 출력
+			for (int i = 0; i < result.size(); i++) {
+				System.out.print(result.get(i) + " ");
+			}
+			System.out.println();
+			return;
+		} else {
+			for (int i = 1; i <= N; i++) {
+				if (!v[i]) {
+
+					result.add(i);
+					v[i] = true;
+
+					bt();
+					result.remove(result.size() - 1);
+					v[i] = false;
+				}
+			}
+		}
+	}
+}
