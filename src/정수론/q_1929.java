f010899dba780ba9c30dd0c90a777faf96f5a5bc
tmdgnsle <rhfqoddl555@gmail.com>
2025-02-25 14:23:34 +0900

    q_1929

--- src/정수론/q_1929.java
+++ src/정수론/q_1929.java
@@ -4,28 +4,35 @@ import java.util.*;
 import java.io.*;
 
 public class q_1929 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int M = sc.nextInt();
-		int N = sc.nextInt();
-		int[] A = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			A[i] = i;
-		}
-
-		for (int i = 2; i <= Math.sqrt(N); i++) {
-			if (A[i] == 0)
-				continue;
-			for (int j = i + i; j <= N; j += i) {
-				A[j] = 0;
-			}
-		}
-
-		for (int i = M; i <= N; i++) {
-			if (A[i] != 0) {
-				System.out.println(A[i]);
-			}
-		}
-
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        boolean[] isPrime = new boolean[N + 1];
+        
+        // 모든 수를 소수로 초기화 (true)
+        Arrays.fill(isPrime, true);
+        
+        // 0과 1은 소수가 아님
+        isPrime[0] = isPrime[1] = false;
+        
+        // 에라토스테네스의 체 알고리즘
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                // i의 배수들을 모두 소수가 아님으로 표시
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        
+        // M부터 N까지의 소수 출력
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                System.out.println(i);
+            }
+        }
+        
+        sc.close();
+    }
 }
