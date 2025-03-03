3af508f4b32d8cf45b9d12993114af5add15fa53
tmdgnsle <rhfqoddl555@gmail.com>
2025-03-03 15:51:46 +0900

    q_1986

--- src/배열2차원/q_1986.java
+++ src/배열2차원/q_1986.java
@@ -1,12 +1,13 @@
-//  체스
+package 배열2차원;
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
 
-    public static void main(String[] args) throws Exception{
+class q_1986 {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
@@ -18,7 +19,7 @@ class Main {
         for (int i = 0; i < kc; i++) {
             int ki = Integer.parseInt(st.nextToken());
             int kj = Integer.parseInt(st.nextToken());
-            chess[ki-1][kj-1] = 1;
+            chess[ki - 1][kj - 1] = 1;
         }
 
         st = new StringTokenizer(br.readLine());
@@ -26,7 +27,7 @@ class Main {
         for (int i = 0; i < qc; i++) {
             int qi = Integer.parseInt(st.nextToken());
             int qj = Integer.parseInt(st.nextToken());
-            chess[qi-1][qj-1] = 2;
+            chess[qi - 1][qj - 1] = 2;
         }
 
         st = new StringTokenizer(br.readLine());
@@ -34,7 +35,7 @@ class Main {
         for (int i = 0; i < pc; i++) {
             int pi = Integer.parseInt(st.nextToken());
             int pj = Integer.parseInt(st.nextToken());
-            chess[pi-1][pj-1] = 3;
+            chess[pi - 1][pj - 1] = 3;
         }
 
         int[] kx = {1, 1, -1, -1, 2, 2, -2, -2};
@@ -44,24 +45,24 @@ class Main {
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(chess[i][j] == 2){
+                if (chess[i][j] == 2) {
                     for (int k = 0; k < 8; k++) {
                         int ni = i + kx[k];
                         int nj = j + ky[k];
 
-                        if(ni < 0 || nj < 0 || ni >=N || nj >=M) continue;
-                        if(chess[ni][nj] != 0) continue;
+                        if (ni < 0 || nj < 0 || ni >= N || nj >= M) continue;
+                        if (chess[ni][nj] != 0) continue;
                         chess[ni][nj] = -1;
                     }
-                } else if(chess[i][j] == 1) {
+                } else if (chess[i][j] == 1) {
                     for (int k = 0; k < 8; k++) {
                         int nx = i;
                         int ny = j;
-                        while(true){
+                        while (true) {
                             nx = nx + qx[k];
                             ny = ny + qy[k];
-                            if(nx < 0 || ny < 0 || nx >= N || ny >= M) break;
-                            if(chess[nx][ny] == 0 || chess[nx][ny] == -1) chess[nx][ny] = -1;
+                            if (nx < 0 || ny < 0 || nx >= N || ny >= M) break;
+                            if (chess[nx][ny] == 0 || chess[nx][ny] == -1) chess[nx][ny] = -1;
                             else break;
 
 
@@ -74,12 +75,11 @@ class Main {
         int count = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(chess[i][j] == 0) count++;
+                if (chess[i][j] == 0) count++;
             }
         }
 
         System.out.println(count);
-
 
 
     }
