f04de31ac05c6f4ea4f236e09322666453ccb0ea
tmdgnsle <rhfqoddl555@gmail.com>
2025-03-03 21:55:24 +0900

    q_16924

--- src/배열2차원/q_16924.java
+++ src/배열2차원/q_16924.java
@@ -1,14 +1,18 @@
+package 배열2차원;
+
+
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class q_16924 {
     static char[][] map;
     static boolean[][] v;
     static int N, M;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     static ArrayList<int[]> arr;
-    public static void main(String[] args) throws Exception{
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
@@ -24,49 +28,49 @@ public class Main{
             for (int j = 0; j < M; j++) {
                 char c = str.charAt(j);
                 map[i][j] = c;
-                if(c == '.') v[i][j] = true;
+                if (c == '.') v[i][j] = true;
             }
         }
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(map[i][j] == '*'){
+                if (map[i][j] == '*') {
 
                     int count = 0;
 
-                    while(true){
+                    while (true) {
                         boolean complete = true;
-                        for (int k = 0; k < count+1; k++) {
+                        for (int k = 0; k < count + 1; k++) {
                             for (int l = 0; l < 4; l++) {
-                                int nx = i + (k+1)*dx[l];
-                                int ny = j + (k+1)*dy[l];
+                                int nx = i + (k + 1) * dx[l];
+                                int ny = j + (k + 1) * dy[l];
 
-                                if(nx < 0 || ny < 0 || nx >=N || ny >= M){
+                                if (nx < 0 || ny < 0 || nx >= N || ny >= M) {
                                     complete = false;
                                     break;
                                 }
 
-                                if(map[nx][ny] != '*'){
+                                if (map[nx][ny] != '*') {
                                     complete = false;
                                     break;
                                 }
                             }
                         }
-                        if(complete) count++;
+                        if (complete) count++;
                         else break;
 
                     }
 
-                    if(count >= 1){
+                    if (count >= 1) {
                         v[i][j] = true;
                         for (int k = 0; k < count; k++) {
                             for (int l = 0; l < 4; l++) {
-                                int nx = i + (k+1)*dx[l];
-                                int ny = j + (k+1)*dy[l];
+                                int nx = i + (k + 1) * dx[l];
+                                int ny = j + (k + 1) * dy[l];
                                 v[nx][ny] = true;
                             }
                         }
-                        arr.add(new int[] {i+1, j+1, count});
+                        arr.add(new int[]{i + 1, j + 1, count});
                     }
 
                 }
@@ -75,16 +79,16 @@ public class Main{
         boolean success = true;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if(!v[i][j]){
+                if (!v[i][j]) {
                     success = false;
                     break;
                 }
             }
         }
 
-        if(success){
+        if (success) {
             System.out.println(arr.size());
-            for (int[] a: arr) {
+            for (int[] a : arr) {
                 System.out.println(a[0] + " " + a[1] + " " + a[2]);
             }
         } else System.out.println(-1);
