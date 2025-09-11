2ef1258e1ec6bb9b21f0464b5a33afcc02191533
이승훈 <rhfqoddl555@gmail.com>
2025-09-11 21:44:24 +0900

    q_23971

--- src/연산자/q_23971.java
+++ src/연산자/q_23971.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+package 연산자;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_23971 {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
