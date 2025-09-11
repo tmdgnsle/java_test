7950660794048d8cd5f99af7e6fd838f2ac2c601
이승훈 <rhfqoddl555@gmail.com>
2025-09-11 21:24:22 +0900

    q_15989

--- src/dp/q_15989.java
+++ src/dp/q_15989.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package dp;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class q_15989 {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
