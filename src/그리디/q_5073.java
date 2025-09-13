33215342676094637a25415804ed056d23cf4920
이승훈 <rhfqoddl555@gmail.com>
2025-09-13 21:10:47 +0900

    q_5073

--- src/그리디/q_5073.java
+++ src/그리디/q_5073.java
@@ -1,17 +1,21 @@
-import java.io.*;
-import java.util.*;
+package 그리디;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class q_5073 {
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        while(true){
+        while (true) {
             st = new StringTokenizer(br.readLine());
             int p1 = Integer.parseInt(st.nextToken());
             int p2 = Integer.parseInt(st.nextToken());
             int p3 = Integer.parseInt(st.nextToken());
-            if(p1 == 0 && p2 == 0 && p3 == 0) break;
+            if (p1 == 0 && p2 == 0 && p3 == 0) break;
 
             int[] p = new int[]{p1, p2, p3};
             Arrays.sort(p);
@@ -20,17 +24,17 @@ public class Main {
             p2 = p[1];
             p3 = p[2];
 
-            if(p1 + p2 <= p3){
+            if (p1 + p2 <= p3) {
                 System.out.println("Invalid");
                 continue;
             }
 
-            if(p1 == p2 && p2 == p3){
+            if (p1 == p2 && p2 == p3) {
                 System.out.println("Equilateral");
                 continue;
             }
 
-            if(p1 == p2 || p2 == p3){
+            if (p1 == p2 || p2 == p3) {
                 System.out.println("Isosceles");
                 continue;
             }
