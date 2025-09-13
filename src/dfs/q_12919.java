7d18b6d349a18b3e50ec05c89505478cd57fde91
이승훈 <rhfqoddl555@gmail.com>
2025-09-13 21:03:53 +0900

    q_12919

--- src/dfs/q_12919.java
+++ src/dfs/q_12919.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+package dfs;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class q_12919 {
+
 
     static String S, T;
     static boolean answer;
@@ -14,29 +17,29 @@ public class Main {
 
         dfs(T);
 
-        if(answer) System.out.println("1");
+        if (answer) System.out.println("1");
         else System.out.println("0");
     }
 
     static void dfs(String word) {
-        if(answer) return; // 이미 답을 찾았으면 더 이상 탐색하지 않음
+        if (answer) return; // 이미 답을 찾았으면 더 이상 탐색하지 않음
 
-        if(word.length() == S.length()) {
-            if(word.equals(S)) {
+        if (word.length() == S.length()) {
+            if (word.equals(S)) {
                 answer = true;
             }
             return;
         }
 
-        if(word.length() < S.length()) return; // 길이가 S보다 작으면 불가능
+        if (word.length() < S.length()) return; // 길이가 S보다 작으면 불가능
 
         // 끝이 A로 끝나는 경우: A를 제거
-        if(word.charAt(word.length() - 1) == 'A') {
+        if (word.charAt(word.length() - 1) == 'A') {
             dfs(word.substring(0, word.length() - 1));
         }
 
         // 시작이 B로 시작하는 경우: 뒤집고 맨 뒤 B 제거
-        if(word.charAt(0) == 'B') {
+        if (word.charAt(0) == 'B') {
             String reversed = new StringBuilder(word).reverse().toString();
             dfs(reversed.substring(0, reversed.length() - 1));
         }
