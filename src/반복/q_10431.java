c836d5a5c4553bb7dbd1d73a112976e68460c476
이승훈 <rhfqoddl555@gmail.com>
2025-09-21 17:21:13 +0900

    q_10431

--- src/반복/q_10431.java
+++ src/반복/q_10431.java
@@ -1,8 +1,14 @@
-import java.io.*;
-import java.util.*;
+package 반복;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_10431 {
+
     static int P;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         P = Integer.parseInt(br.readLine());
@@ -16,15 +22,15 @@ public class Main {
                 int height = Integer.parseInt(st.nextToken());
                 int nextIdx = j;
                 for (int k = 0; k < j; k++) {
-                    if(order[k] > height){
+                    if (order[k] > height) {
                         nextIdx = k;
                         break;
                     }
                 }
 
-                if(nextIdx != j){
+                if (nextIdx != j) {
                     for (int k = j; k > nextIdx; k--) {
-                        order[k] = order[k-1];
+                        order[k] = order[k - 1];
                         count++;
                     }
                 }
