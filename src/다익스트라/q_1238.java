21c34943c5e46a72db2d6e2b3d6969c9b77df911
이승훈 <rhfqoddl555@gmail.com>
2025-09-25 20:44:32 +0900

    q_1238

--- src/다익스트라/q_1238.java
+++ src/다익스트라/q_1238.java
@@ -1,32 +1,42 @@
-import java.io.*;
-import java.util.*;
+package 다익스트라;
 
-public class Main {
-    static class Node implements Comparable<Node>{
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class q_1238 {
+
+    static class Node implements Comparable<Node> {
         int point;
         int distance;
 
-        public Node(int point, int distance){
+        public Node(int point, int distance) {
             this.point = point;
             this.distance = distance;
         }
 
         @Override
-        public int compareTo(Node o){
+        public int compareTo(Node o) {
             return this.distance - o.distance;
         }
     }
+
     static int N, M, X;
     static ArrayList<Node>[] arrs;
     static int[] distances;
     static boolean[] v;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-        arrs = new ArrayList[N+1];
+        arrs = new ArrayList[N + 1];
         for (int i = 0; i <= N; i++) {
             arrs[i] = new ArrayList<>();
         }
@@ -41,20 +51,19 @@ public class Main {
         }
 
 
+        int[] totalDistances = new int[N + 1];
 
-        int[] totalDistances = new int[N+1];
-
-        for(int i = 1; i<=N; i++){
-            distances = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            distances = new int[N + 1];
             Arrays.fill(distances, Integer.MAX_VALUE);
             distances[i] = 0;
-            v = new boolean[N+1];
+            v = new boolean[N + 1];
             djikstra(i);
-            if(i == X){
-                for(int j = 1; j<=N; j++){
+            if (i == X) {
+                for (int j = 1; j <= N; j++) {
                     totalDistances[j] += distances[j];
                 }
-            }else {
+            } else {
                 totalDistances[i] += distances[X];
             }
         }
@@ -62,8 +71,8 @@ public class Main {
         int maxDistance = Integer.MIN_VALUE;
 
 
-        for(int i = 1; i<=N; i++){
-            if(totalDistances[i] > maxDistance){
+        for (int i = 1; i <= N; i++) {
+            if (totalDistances[i] > maxDistance) {
                 maxDistance = totalDistances[i];
             }
         }
@@ -72,22 +81,22 @@ public class Main {
 
     }
 
-    static void djikstra(int start){
+    static void djikstra(int start) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(start, 0));
-        while(!pq.isEmpty()){
+        while (!pq.isEmpty()) {
             Node current = pq.poll();
             int cp = current.point;
             int cd = current.distance;
 
-            if(v[cp]) continue;
+            if (v[cp]) continue;
             v[cp] = true;
 
-            for(Node node: arrs[cp]){
+            for (Node node : arrs[cp]) {
                 int next = node.point;
                 int nd = node.distance;
 
-                if(cd + nd < distances[next]){
+                if (cd + nd < distances[next]) {
                     distances[next] = cd + nd;
                     pq.offer(new Node(next, distances[next]));
                 }
