0aa7af1c51d8651e12a0105235c109118eb8b8d4
이승훈 <rhfqoddl555@gmail.com>
2025-10-04 22:33:53 +0900

    q_9935

--- src/스택/q_9935.java
+++ src/스택/q_9935.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package 스택;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class q_9935 {
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
