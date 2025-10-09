1164c848618a414d8e95f4443448f19511eeab83
이승훈 <rhfqoddl555@gmail.com>
2025-10-09 17:04:35 +0900

    q_2668

--- src/dfs/q_2668.java
+++ src/dfs/q_2668.java
@@ -1,7 +1,12 @@
-import java.io.*;
-import java.util.*;
+package dfs;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class q_2668 {
+
     static int N;
     static int[] next;          // next[i] = i가 가리키는 숫자 a_i
     static boolean[] visited;   // DFS 방문 여부
