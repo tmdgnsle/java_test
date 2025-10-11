181da12878b425e434a16386ae4636d0c78058e1
이승훈 <rhfqoddl555@gmail.com>
2025-10-11 16:52:52 +0900

    q_1863

--- src/스택/q_1863.java
+++ src/스택/q_1863.java
@@ -1,7 +1,13 @@
-import java.io.*;
-import java.util.*;
+package 스택;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class q_1863 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
