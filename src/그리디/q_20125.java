bb57cb15945018a408fc9366646981b4b0970764
이승훈 <rhfqoddl555@gmail.com>
2025-11-24 14:41:43 +0900

    q_20125

--- src/그리디/q_20125.java
+++ src/그리디/q_20125.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package 그리디;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q_20125 {
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
