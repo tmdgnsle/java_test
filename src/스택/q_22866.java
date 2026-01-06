db81070a3456592d9a372de7d0876f7b81ec571c
이승훈 <rhfqoddl555@gmail.com>
2026-01-06 17:44:10 +0900

    q_22866

--- src/스택/q_22866.java
+++ src/스택/q_22866.java
@@ -1,7 +1,13 @@
-import java.io.*;
-import java.util.*;
+package 스택;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class q_22866 {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
