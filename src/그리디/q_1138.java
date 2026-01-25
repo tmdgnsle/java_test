800692460107df6e935db8e687f68fade2ad9e54
이승훈 <rhfqoddl555@gmail.com>
2026-01-25 17:04:00 +0900

    q_1138

--- src/그리디/q_1138.java
+++ src/그리디/q_1138.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package 그리디;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_1138 {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine().trim());
