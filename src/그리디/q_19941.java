71588b5a7fcecfdf75ebb006ef1f613461f90e32
이승훈 <rhfqoddl555@gmail.com>
2026-01-27 12:32:02 +0900

    q_19941

--- src/그리디/q_19941.java
+++ src/그리디/q_19941.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package 그리디;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_19941 {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -9,18 +13,18 @@ public class Main {
         int K = Integer.parseInt(st.nextToken());
         char[] table = new char[N];
         String str = br.readLine();
-        for(int i = 0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             table[i] = str.charAt(i);
         }
         boolean[] can = new boolean[N];
 
         int answer = 0;
-        for(int i = 0; i<N; i++){
-            if(table[i] == 'P'){
-                for(int j = i - K; j<= i + K; j++){
-                    if(j < 0 || j >= N) continue;
+        for (int i = 0; i < N; i++) {
+            if (table[i] == 'P') {
+                for (int j = i - K; j <= i + K; j++) {
+                    if (j < 0 || j >= N) continue;
 
-                    if(table[j] == 'H' && !can[j]){
+                    if (table[j] == 'H' && !can[j]) {
                         can[j] = true;
                         answer++;
                         break;
