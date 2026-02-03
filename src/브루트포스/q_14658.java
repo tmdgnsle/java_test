af7c4034780aebdc33279aa5073d1d360dd419f1
이승훈 <rhfqoddl555@gmail.com>
2026-02-03 16:41:56 +0900

    q_14658

--- src/브루트포스/q_14658.java
+++ src/브루트포스/q_14658.java
@@ -1,12 +1,18 @@
-import java.io.*;
-import java.util.*;
+package 브루트포스;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class q_14658 {
+
     static int N, M, L, K;
     static ArrayList<Integer> xSet;
     static ArrayList<Integer> ySet;
     static ArrayList<int[]> stars;
     static int tStars;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -19,15 +25,15 @@ public class Main {
         ySet = new ArrayList<>();
         stars = new ArrayList<>();
 
-        for(int i = 0; i<K; i++){
+        for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
             int p1 = Integer.parseInt(st.nextToken());
             int p2 = Integer.parseInt(st.nextToken());
 
-            if(!xSet.contains(p1)) xSet.add(p1);
-            if(!ySet.contains(p2)) ySet.add(p2);
+            if (!xSet.contains(p1)) xSet.add(p1);
+            if (!ySet.contains(p2)) ySet.add(p2);
 
-            stars.add(new int[] {p1, p2});
+            stars.add(new int[]{p1, p2});
         }
 
         tStars = Integer.MIN_VALUE;
@@ -39,9 +45,9 @@ public class Main {
     }
 
 
-    static void find(){
-        for(int i = 0; i<xSet.size(); i++){
-            for(int j = 0; j<ySet.size(); j++){
+    static void find() {
+        for (int i = 0; i < xSet.size(); i++) {
+            for (int j = 0; j < ySet.size(); j++) {
                 int x = xSet.get(i);
                 int y = ySet.get(j);
 
@@ -50,17 +56,17 @@ public class Main {
         }
     }
 
-    static void count(int x, int y){
+    static void count(int x, int y) {
         int maxX = x + L;
         int maxY = y + L;
 
-        if(maxX > N) maxX = N;
-        if(maxY > M) maxY = M;
+        if (maxX > N) maxX = N;
+        if (maxY > M) maxY = M;
 
         int count = 0;
 
-        for(int[] star: stars){
-            if(star[0] >= x && star[0] <= maxX && star[1] >= y && star[1] <= maxY) count++;
+        for (int[] star : stars) {
+            if (star[0] >= x && star[0] <= maxX && star[1] >= y && star[1] <= maxY) count++;
         }
 
         tStars = Math.max(tStars, count);
