a062a5b7ea19d807db22d7798b6112b4e9eb4d81
tmdgnsle <rhfqoddl555@gmail.com>
2026-03-06 14:01:39 +0900

    q_2304

--- src/브루트포스/q_2304.java
+++ src/브루트포스/q_2304.java
@@ -1,27 +1,33 @@
-import java.io.*;
-import java.util.*;
+package 브루트포스;
 
-public class Main {
-    static class Node implements Comparable<Node>{
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class q_2304 {
+
+    static class Node implements Comparable<Node> {
         int L;
         int H;
 
-        Node(int L, int H){
+        Node(int L, int H) {
             this.L = L;
             this.H = H;
         }
 
         @Override
-        public int compareTo(Node o){
+        public int compareTo(Node o) {
             return this.L - o.L;
         }
     }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
         Node[] nodes = new Node[N];
-        for(int i = 0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int l = Integer.parseInt(st.nextToken());
             int h = Integer.parseInt(st.nextToken());
@@ -32,8 +38,8 @@ public class Main {
 
         int maxIdx = 0;
         int maxHeight = 0;
-        for(int i = 0; i<N; i++){
-            if(nodes[i].H > maxHeight){
+        for (int i = 0; i < N; i++) {
+            if (nodes[i].H > maxHeight) {
                 maxHeight = nodes[i].H;
                 maxIdx = i;
             }
@@ -42,11 +48,11 @@ public class Main {
         int currentLeft = 0;
         int beforeX = -1;
         int answer = 0;
-        for(int i = 0; i<=maxIdx; i++){
+        for (int i = 0; i <= maxIdx; i++) {
             int currentH = nodes[i].H;
             int currentL = nodes[i].L;
 
-            if(currentH < currentLeft) continue;
+            if (currentH < currentLeft) continue;
 
             answer += (currentL - beforeX) * currentLeft;
 
@@ -56,11 +62,11 @@ public class Main {
 
         int currnetRight = 0;
         beforeX = -1;
-        for(int i = N-1; i>=maxIdx; i--){
+        for (int i = N - 1; i >= maxIdx; i--) {
             int currentH = nodes[i].H;
             int currentL = nodes[i].L;
 
-            if(currentH < currnetRight) continue;
+            if (currentH < currnetRight) continue;
 
             answer += (beforeX - currentL) * currnetRight;
 
@@ -72,8 +78,6 @@ public class Main {
         answer += maxHeight;
 
         System.out.println(answer);
-
-
 
 
     }
