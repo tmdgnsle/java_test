b0c1c2e466539990af1856753a79739becb37ddd
tmdgnsle <rhfqoddl555@gmail.com>
2026-03-07 18:47:11 +0900

    Q_2607

--- src/그리디/q_2607.java
+++ src/그리디/q_2607.java
@@ -1,7 +1,10 @@
-import java.io.*;
-import java.util.*;
+package 그리디;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class q_2607 {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -11,29 +14,29 @@ public class Main {
 
         int[] alpha = new int[26];
         String str = br.readLine();
-        for(char c: str.toCharArray()){
+        for (char c : str.toCharArray()) {
             int idx = c - 'A';
             alpha[idx]++;
         }
         int length = str.length();
 
-        for(int i = 0; i<N-1; i++){
+        for (int i = 0; i < N - 1; i++) {
             int[] targetAlpha = new int[26];
             String target = br.readLine();
             int targetLength = target.length();
-            if(Math.abs(length - targetLength) >= 2) continue;
-            for(char c: target.toCharArray()){
+            if (Math.abs(length - targetLength) >= 2) continue;
+            for (char c : target.toCharArray()) {
                 int idx = c - 'A';
                 targetAlpha[idx]++;
             }
 
             int diff = 0;
 
-            for(int j = 0; j<26; j++){
+            for (int j = 0; j < 26; j++) {
                 diff += Math.abs(targetAlpha[j] - alpha[j]);
             }
 
-            if(diff <= 2) answer++;
+            if (diff <= 2) answer++;
 
         }
         System.out.println(answer);
