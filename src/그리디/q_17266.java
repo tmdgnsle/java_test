a57b2b16e63c4804fa31549a6cf0668f6f43d6ab
tmdgnsle <rhfqoddl555@gmail.com>
2026-03-07 16:34:17 +0900

    q_17266

--- src/그리디/q_17266.java
+++ src/그리디/q_17266.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package 그리디;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_17266 {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
