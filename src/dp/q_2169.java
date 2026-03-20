79afbcb17ae1fe0f1aacc7f4e2a53a3afbe9c78c
tmdgnsle <rhfqoddl555@gmail.com>
2026-03-20 18:08:46 +0900

    q_2169

--- src/dp/q_2169.java
+++ src/dp/q_2169.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.util.*;
+package dp;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class q_2169 {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -10,9 +14,9 @@ public class Main {
         int M = Integer.parseInt(st.nextToken());
 
         int[][] map = new int[N][M];
-        for(int i = 0; i<N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<M; j++){
+            for (int j = 0; j < M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
@@ -20,32 +24,32 @@ public class Main {
         int[][] dp = new int[N][M];
 
         dp[0][0] = map[0][0];
-        for(int i = 1; i<M; i++){
-            dp[0][i] = dp[0][i-1] + map[0][i];
+        for (int i = 1; i < M; i++) {
+            dp[0][i] = dp[0][i - 1] + map[0][i];
         }
 
-        for(int i = 1; i<N; i++){
+        for (int i = 1; i < N; i++) {
             int[] left = new int[M];
             int[] right = new int[M];
 
             // 왼쪽 -> 오른쪽
-            left[0] = dp[i-1][0] + map[i][0];
-            for(int j = 1; j<M; j++){
-                left[j] = Math.max(left[j-1], dp[i-1][j]) + map[i][j];
+            left[0] = dp[i - 1][0] + map[i][0];
+            for (int j = 1; j < M; j++) {
+                left[j] = Math.max(left[j - 1], dp[i - 1][j]) + map[i][j];
             }
 
             // 오른쪽 -> 왼쪽
-            right[M-1] = dp[i-1][M-1] + map[i][M-1];
-            for(int j = M-2; j>=0; j--){
-                right[j] = Math.max(right[j+1], dp[i-1][j]) + map[i][j];
+            right[M - 1] = dp[i - 1][M - 1] + map[i][M - 1];
+            for (int j = M - 2; j >= 0; j--) {
+                right[j] = Math.max(right[j + 1], dp[i - 1][j]) + map[i][j];
             }
 
-            for(int j = 0; j<M; j++){
+            for (int j = 0; j < M; j++) {
                 dp[i][j] = Math.max(left[j], right[j]);
             }
         }
 
-        System.out.println(dp[N-1][M-1]);
+        System.out.println(dp[N - 1][M - 1]);
 
     }
 }
