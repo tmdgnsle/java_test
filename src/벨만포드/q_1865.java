594340f89ab4987d50dcc383a82fdb19209e206e
tmdgnsle <rhfqoddl555@gmail.com>
2026-04-26 22:06:18 +0900

    q_1865

--- src/벨만포드/q_1865.java
+++ src/벨만포드/q_1865.java
@@ -1,7 +1,14 @@
-import java.io.*;
-import java.util.*;
+package 벨만포드;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class q_1865 {
+
 
     static class Edge {
         int from, to, cost;
